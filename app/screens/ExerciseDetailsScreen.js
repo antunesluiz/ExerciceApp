cc7af309c2678804a450aa90fb550a6a01e36972
Luiz Antunes <antunesluiz27@gmail.com>
2023-01-08 17:27:39 -0300

    rai lorena

---
+++ app/screens/ExerciseDetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ExerciseDetailsScreen = () => {
+  return (
+    <View>
+      <Text>ExerciseDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default ExerciseDetailsScreen;
---
+++ app/screens/ExerciseHomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ExerciseHomeScreen = () => {
+  return (
+    <View>
+      <Text>ExerciseHomeScreen</Text>
+    </View>
+  );
+};
+
+export default ExerciseHomeScreen;
